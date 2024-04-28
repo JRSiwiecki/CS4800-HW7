88e076f1924f427b75fae82bdcdbab2e50779f86
JRSiwiecki <JRSiwiecki@gmail.com>
2024-04-27 20:04:48 -0700

    Implement SongServer

---
+++ src/main/java/org/proxy/SongServer.java
@@ -0,0 +1,52 @@
+package org.proxy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SongServer implements SongService
+{
+    private final ArrayList<Song> songs = new ArrayList<>();
+
+    public void addSong(Song song)
+    {
+        songs.add(song);
+    }
+
+    @Override
+    public Song searchById(Integer songID)
+    {
+        return songs.get(songID);
+    }
+
+    @Override
+    public List<Song> searchByTitle(String title)
+    {
+        ArrayList<Song> matchingSongs = new ArrayList<>();
+
+        for (Song song : songs)
+        {
+            if (song.getTitle().equals(title))
+            {
+                matchingSongs.add(song);
+            }
+        }
+
+        return matchingSongs;
+    }
+
+    @Override
+    public List<Song> searchByAlbum(String album)
+    {
+        ArrayList<Song> matchingSongs = new ArrayList<>();
+
+        for (Song song : songs)
+        {
+            if (song.getAlbum().equals(album))
+            {
+                matchingSongs.add(song);
+            }
+        }
+
+        return matchingSongs;
+    }
+}
