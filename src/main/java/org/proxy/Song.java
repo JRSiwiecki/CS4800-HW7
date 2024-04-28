5680bd276286264f2e2618738e484e464f8c6fd6
JRSiwiecki <JRSiwiecki@gmail.com>
2024-04-27 19:56:34 -0700

    Add Song

---
+++ src/main/java/org/proxy/Song.java
@@ -0,0 +1,37 @@
+package org.proxy;
+
+public class Song
+{
+    private final String title;
+    private final String artist;
+    private final String album;
+    private final int duration;
+
+    public Song(String title, String artist, String album, int duration)
+    {
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+        this.duration = duration;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getArtist()
+    {
+        return artist;
+    }
+
+    public String getAlbum()
+    {
+        return album;
+    }
+
+    public int getDuration()
+    {
+        return duration;
+    }
+}
